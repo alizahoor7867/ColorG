7b6b084f9b288f961bb5735cbeadb6afad9f294c
alizahoor7867 <144946536+alizahoor7867@users.noreply.github.com>
2025-02-09 16:18:44 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Inputs from './Control_inputs/Inputs'
 import { Toaster } from 'react-hot-toast'
 import Generater from './loremGenerater/Generater'
-import Color from '/ColorGenerater/color';
+import Color from 'ColorGenerater/color';
 
 const App = () => {
   return (
